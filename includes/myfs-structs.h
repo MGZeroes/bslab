95a3adb4d623c175e698ca8e2c51134c497f4737
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2022-12-22 03:45:50 +0100

    added struct MyFsMemoryInfo

--- includes/myfs-structs.h
+++ includes/myfs-structs.h
@@ -14,6 +14,21 @@
 #define NUM_DIR_ENTRIES 64
 #define NUM_OPEN_FILES 64
 
+#include <string>
+
+using namespace std;
+
 // TODO: Add structures of your file system here
+struct MyFsMemoryInfo {
+    char name[NAME_LENGTH + 1];     // Path to the file    256bit
+    size_t size;                    // Data Size    64bit
+    string content;                     // Data
+    __uid_t uid;                    // User ID      32bit
+    __gid_t gid;                    // Gruppen ID   32bit
+    __mode_t mode;                  // File mode    32bit
+    __time_t atime;                 // Time of last access.         64bit
+    __time_t mtime;                 // Time of last modification.   64bit
+    __time_t ctime;                 // Time of last status change.  64bit
+};
 
 #endif /* myfs_structs_h */
