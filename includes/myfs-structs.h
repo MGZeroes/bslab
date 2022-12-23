34602da4e76b47182ac70949ceedc6c08477fd40
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2022-12-23 04:34:30 +0100

    removed unnecessary MyFsMemoryInfo members

--- includes/myfs-structs.h
+++ includes/myfs-structs.h
@@ -20,15 +20,13 @@ using namespace std;
 
 // TODO: Add structures of your file system here
 struct MyFsMemoryInfo {
-    char name[NAME_LENGTH + 1];     // Path to the file    256bit
-    size_t size;                    // Data Size    64bit
-    string content;                     // Data
-    __uid_t uid;                    // User ID      32bit
-    __gid_t gid;                    // Gruppen ID   32bit
-    __mode_t mode;                  // File mode    32bit
-    __time_t atime;                 // Time of last access.         64bit
-    __time_t mtime;                 // Time of last modification.   64bit
-    __time_t ctime;                 // Time of last status change.  64bit
+    string content;                 // Data
+    __uid_t uid;                    // User ID
+    __gid_t gid;                    // Gruppen ID
+    __mode_t mode;                  // File mode
+    __time_t  atime;          // Time of last access.
+    __time_t  mtime;          // Time of last modification.
+    __time_t  ctime;          // Time of last status change.
 };
 
 #endif /* myfs_structs_h */
