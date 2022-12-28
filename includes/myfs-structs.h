0a571a1cbfc2f84f669279fe56301866418717c7
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2022-12-28 05:25:56 +0100

    added MyFsDiskInfo struct

--- includes/myfs-structs.h
+++ includes/myfs-structs.h
@@ -33,4 +33,17 @@ struct MyFsMemoryInfo {
     __time_t  ctime; // Time of last status change
 };
 
+struct MyFsDiskInfo {
+
+    // File metadata
+    size_t size; // Data Size   64bit
+    int32_t data; // Block Pos  32bit
+    __uid_t uid; // User ID     32bit
+    __gid_t gid; // Gruppen ID  32bit
+    __mode_t mode; // File mode 32bit
+    __time_t atime; // Time of last access.         64bit
+    __time_t mtime; // Time of last modification.   64bit
+    __time_t ctime; // Time of last status change.  64bit
+};
+
 #endif /* myfs_structs_h */
