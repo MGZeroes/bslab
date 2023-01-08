09790576ed19a5bf38dd74618263b03707dca3d1
MGZeroes <36378780+MGZeroes@users.noreply.github.com>
2023-01-08 18:30:20 +0100

    added read and write methods for the Root

--- includes/myondiskfs.h
+++ includes/myondiskfs.h
@@ -188,6 +188,78 @@ private:
         return 0;
     }
 
+    int readRoot(int fd) {
+
+        // Clear the root map before it gets read
+        root.clear();
+
+        // Read the blocks of the Root to the file system
+        for (int i = 0; i < NUM_DIR_ENTRIES; i++) {
+
+            // Allocate a buffer for the FAT
+            char *buffer = (char*) malloc(BLOCK_SIZE);
+            memset(buffer, 0, BLOCK_SIZE);
+
+            // Write the entries to the buffer
+            this->blockDevice->read(i + superBlock.rootBlockOffset, buffer);
+
+            // Copy the buffer into the entries for this block
+            MyFsDiskInfo file;
+            memcpy(&file, buffer, sizeof(MyFsDiskInfo));
+
+            // Write the key with a slash for easier path finding
+            if(strcmp(file.name, "") != 0) {
+                string key = "/";
+                key.append(file.name);
+                root.emplace(key, file);
+            }
+
+            // Free the buffer
+            free(buffer);
+        }
+
+    }
+
+    int writeRoot(int fd) {
+
+        // Clear the root container before it gets written
+        for (int i = 0; i < NUM_DIR_ENTRIES; ++i) {
+
+            // Allocate a buffer for the Root
+            char *buffer = (char*) malloc(BLOCK_SIZE);
+            memset(buffer, 0, BLOCK_SIZE);
+
+            // Clear the block
+            this->blockDevice->write(i + superBlock.rootBlockOffset, buffer);
+
+            // Free the buffer
+            free(buffer);
+        }
+
+        // Write root
+        size_t index = 0;
+        for (const auto& entry : root) {
+
+            // Allocate a buffer for the Root
+            char *buffer = (char*) malloc(BLOCK_SIZE);
+            memset(buffer, 0, BLOCK_SIZE);
+
+            // Copy the entries for this block into the buffer
+            memcpy(buffer, &entry.second, sizeof(MyFsDiskInfo));
+
+            // Write the entries to the block
+            this->blockDevice->write(index + superBlock.rootBlockOffset, buffer);
+
+            // Free the buffer
+            free(buffer);
+
+            index++;
+
+            if(index >= NUM_DIR_ENTRIES)
+                break;
+        }
+    }
+
 };
 
 #endif //MYFS_MYONDISKFS_H
